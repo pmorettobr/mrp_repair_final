f0d21dfd09ca16349be9e63ee767e552e201214e
pmorettobr <pmorettobr@gmail.com>
2026-03-16 16:48:09 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -2,8 +2,20 @@
     'name': 'Repair + MRP Integration',
     'version': '16.0.1.0.0',
     'summary': 'Integra Repair Order com MRP para fluxo híbrido com subprodutos Presonalizacao para WTT-Paulmar',
-    'author': 'Paulo Dev',
+    'description': """
+        Integra Repair Order com MRP para fluxo híbrido:
+            - Nova Ordem de Reparo
+            - Integrar WorkOrder do MRP no Repair
+            - Barra de Processo 
+            - Campos Personalizados
+            - Relatorios
+            
+    """,
+    'author': 'Paulo Moretto',
+    'website": "https://github.com/pmoretto',
+    "category": "MRP/Repair",
     'depends': ['repair', 'mrp'],
+    'license': 'LGPL-3',
     'data': [
         'views/repair_order_views.xml',
         'views/mrp_production_views.xml',
